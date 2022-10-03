d3da9437a21274b1e38d19fffad86c0d8edce68f
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-03 19:33:35 +0530

    Create 5Q.c

---
+++ 5Q.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int var;
+    printf("Enter the value of var");
+    scanf("%d",&var);
+    switch(var){
+        case 1:
+            printf("good");
+            break;
+        case 2:
+            printf("better");
+            break;
+        case 3:
+            printf("best");
+            break;
+        default : printf("Invalid"); 
+    }
+    return 0;
+}
